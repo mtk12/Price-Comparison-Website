3fc15509b66724793b2246c83658b31e3a27fe15
Muhammad Taha Khan <mtahakhan93@gmail.com>
2020-06-09 15:35:48 +0500

    initial commit

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
-driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 #chrome_options = webdriver.ChromeOptions()
 #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
