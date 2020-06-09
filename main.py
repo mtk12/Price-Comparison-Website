41baeea70d7e9e26e692c3e2952c3bd0cca58a6b
Muhammad Taha Khan <mtahakhan93@gmail.com>
2020-06-09 15:47:42 +0500

    initial commit

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 #driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = CHROMEDRIVER_PATH
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("headless")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=GOOGLE_CHROME_PATH, chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 #option = webdriver.ChromeOptions()
 #option.add_argument('headless')
